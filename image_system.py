fabef8c9b6c3cc3f4c7896e27664da3512c5d8b3
itomasaki <l1sum@ito.local>
2019-07-20 12:28:38 +0900

    make base

--- image_system.py
+++ image_system.py
@@ -1,6 +1,55 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
+
+class ImageSystem(Node):
+    def __init__(self):
+        super(ImageSystem, self).__init__("ImageSystem")
+
+        self.create_subscription(String, "/image_system/command", self.command_callback)
+
+
+    def command_callback(self, msg):
+        if self.one_time_execute(msg.data, self.command):
+
+            # contain command data
+            self.command = msg.data
+
+            # Command:speak , Content:hello!
+            command = msg.data.split(" , ")
+
+            if "human" == command[0].replace("Command:", ""):
+                self.detect_human()
+
+            if "sex" == command[0].replace("Command:", ""):
+                self.detect_sex()
+
+            if "object" == command[0].replace("Command:", ""):
+                self.detect_object()
+
+
+    def detect_human(self):
+        print("[*] detect human")
+
+    def detect_sex(self):
+        print("[*] detect sex")
+
+    def detect_object(self):
+        print("[*] detect object")
+
+
+    # only one time execute
+    def one_time_execute(self, now, previous):
+        flag = False
+
+        if now != previous:
+            flag = True
+
+        return flag
+
+
 def main():
     rclpy.init()
-
+    node = ImageSystem()
+    rclpy.spin(node)
