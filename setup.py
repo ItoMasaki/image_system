7d5c87beba9523a10755ad1b490172a4914839b5
yamatomatsuda <yamato19990708@gmail.com>
2019-08-07 08:09:03 +0900

    重すぎてbuild不可なのでコメントアウト,適宜移動させれば利用可能

--- setup.py
+++ setup.py
@@ -9,15 +9,30 @@ setup(
     packages=[],
     py_modules=[
         'image_system',
-        'detect_human/HandNet',
-        'detect_human/FaceNet',
-        'detect_human/CocoPoseNet',
-        'detect_human/entity',
-        'detect_human/pose_detector',
-        'detect_human/detect_human',
-        'detect_sex/detect_sex',
-        'detect_sex/classifier',
-        'detect_sex/model',
+    ],
+    data_files=[
+        ('lib/' + package_name, ['package.xml']),
+        ('lib/' + package_name+'/detect_modules/detect_human',
+         ['detect_modules/detect_human/CocoPoseNet.py',
+          'detect_modules/detect_human/FaceNet.py',
+          'detect_modules/detect_human/HandNet.py',
+          'detect_modules/detect_human/detect_human.py',
+          'detect_modules/detect_human/entity.py',
+          'detect_modules/detect_human/pose_detector.py',
+          ]),
+        ('lib/'+package_name+'/detect_modules/detect_sex',
+         ['detect_modules/detect_sex/classifier.py',
+          'detect_modules/detect_sex/detect_sex.py',
+          'detect_modules/detect_sex/model.py'
+          ]),
+"""
+        ('lib/'+package_name+'/model'),
+        [
+            'model/cifier_adam.npz',
+            'model/coco_posenet.npz',
+            'model/haarcascade_frontalface_alt.xml'
+        ]
+"""
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -37,9 +52,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'image_system = image_system:main',
-            'detect_human = detect_human.detect_human:main',
-            'detect_sex = detect_sex.detect_sex:main',
+            'image_system=image_system:main'
         ],
     },
 )
