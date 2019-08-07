c717b256561b959d48ad3a6f3b48e4445358ea53
l1sum <l1sum@icloud.com>
2019-08-07 18:07:41 +0900

    mada kannsei siteinai

--- get_npz.bash
+++ get_npz.bash
@@ -1,7 +1,11 @@
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bn-zEOL1hsUwI1Hd4aS1LjUQsFYo4yp0" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bn-zEOL1hsUwI1Hd4aS1LjUQsFYo4yp0" -O /model/coco_posenet.npz
+FILE_ID=1bn-zEOL1hsUwI1Hd4aS1LjUQsFYo4yp0
+FILE_NAME=./model/coco_posenet.npz
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" > /dev/null
+wget --save-cookie /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" -O /model/cifier_adam.npz
+aria2c -x10 --load-cookies /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ./model/coco_posenet.npz
+rm -rf 'uc?export*'
+
+#curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" > /dev/null
+#CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+#curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" -O ./model/cifier_adam.npz
