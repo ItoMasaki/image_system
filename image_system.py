5e98198ceb419f90b60855bdc4f390bec2113dc1
l1sum <l1sum@icloud.com>
2019-08-07 10:12:24 +0900

    don't destroy nodegit add image_system.py ! And, implimented get_image function

--- image_system.py
+++ image_system.py
@@ -1,4 +1,3 @@
-"""image system main code"""
 from detect_modules.detect_human import detect_human
 
 from detect_modules.detect_sex import detect_sex
@@ -8,19 +7,23 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 
 from std_msgs.msg import String
+from sensro_msgs.msg import Image
+
+from cv_bridge import CvBridge
 
 
 class ImageSystem(Node):
     def __init__(self):
         super(ImageSystem, self).__init__('ImageSystem')
 
-        self.create_subscription(
-            String,
-            '/image_system/command',
-            self.command_callback,
-            qos_profile_sensor_data)
+        self.senses_publisher = self.create_publisher(String, 'Cerebrum/Command', qos_profile_sensor_data)
+        self.create_subscription(String, '/image_system/command', self.command_callback, qos_profile_sensor_data)
+        self.create_subscription(Image, '/camera/color/image_raw', self.get_image, qos_profile_sensor_data)
         self.message = None
         self.command = None
+        self._trans_message = String()
+
+        self.bridge = CvBridge()
 
     def command_callback(self, msg):
         if self.one_time_execute(msg.data, self.command):
@@ -40,31 +43,36 @@ class ImageSystem(Node):
             if 'object' == command[0].replace('Command:', ''):
                 self.detect_object()
 
+
+    # detect number's human
     def detect_human(self):
-        # [TODO] input image array (numpy)
-        number = detect_human(image)
-        return str(number)
+        number = detect_human(self.image)
+        return number
 
+
+    # detect human sex
     def detect_sex(self):
-        # [TODO] input image array (numpy)
-        woman, man = detect_sex(image)
-        return str(woman)+'|'+str(man)
+        woman, man = detect_sex(self.image)
+        return woman + '|' + man
 
+
+    # [FUTURE] detect object with YOLO, SSD, R-DAD 
     def detect_object(self):
         # [TODO] sonouti yaru!
         print('[*] detect object')
 
+    
+    # send data to cerebrum
     def cerebrum_publisher(self, message):
-        self.senses_publisher = self.create_publisher(
-            String,
-            'Cerebrum/Command',
-            qos_profile_sensor_data)
 
-        _trans_message = String()
-        _trans_message.data = message
+        self._trans_message.data = message
+        self.senses_publisher.publish(self._trans_message)
 
-        self.senses_publisher.publish(_trans_message)
-        self.destroy_publisher(self.senses_publisher)
+
+    # get image from realsense
+    def get_image(self, msg):
+        self.image = self.bridge.imgmsg_to_cv2(msg)
+
 
     # only one time execute
     def one_time_execute(self, now, previous):
