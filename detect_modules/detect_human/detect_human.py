2e4ed615969d7dc757284f015ee5bd721b5a0153
l1sum <l1sum@icloud.com>
2019-08-07 10:52:00 +0900

    change path

--- detect_modules/detect_human/detect_human.py
+++ detect_modules/detect_human/detect_human.py
@@ -8,16 +8,14 @@ from .pose_detector import PoseDetector, draw_person_pose
 chainer.using_config('enable_backprop', False)
 
 file_path = os.path.abspath(__file__)
-npz = file_path.replace(
-    'detect_modules/detect_human/detect_human.py', 'model/coco_posenet.npz')
+#npz = file_path.replace(
+#    'detect_modules/detect_human/detect_human.py', 'model/coco_posenet.npz')
+npz_path = "image_system/model/coco_posenet.npz"
 device_number = -1
-pose_detector = PoseDetector(
-    'posenet',
-    npz,
-    device=device_number)
+pose_detector = PoseDetector('posenet', npz_path, device=device_number)
 
 
 # input image array
 def detect_human(image):
     person_pose_array, _ = pose_detector(image)
-    return len(person_pose_array)
+    return str(len(person_pose_array))
