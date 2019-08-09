a04afd17cf3181649718d26e70d807becd5e8b43
l1sum <l1sum@icloud.com>
2019-08-09 18:22:14 +0900

    implimented

--- get_npz.bash
+++ get_npz.bash
@@ -1,11 +1,30 @@
 FILE_ID=1bn-zEOL1hsUwI1Hd4aS1LjUQsFYo4yp0
-FILE_NAME=./model/coco_posenet.npz
+FILE_NAME=coco_posenet.npz.zip
 
-wget --save-cookie /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-aria2c -x10 --load-cookies /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ./model/coco_posenet.npz
-rm -rf 'uc?export*'
+curl -c /tmp/cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" |grep confirm |  sed -e "s/^.*confirm=\(.*\)&amp;id=.*$/\1/" | xargs -I{} \
+curl -b /tmp/cookie.txt  -L -o ${FILE_NAME} "https://drive.google.com/uc?confirm={}&export=download&id=${FILE_ID}"
+unzip ${FILE_NAME}
+mv coco_posenet.npz model
+rm -rf ${FILE_NAME}
 
-#curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" > /dev/null
-#CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-#curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759" -O ./model/cifier_adam.npz
+
+FILE_ID=1osfe6mWT4dlnmcUaIINZYKvIkqwVi759
+FILE_NAME=cifier_adam.npz.zip
+
+curl -c /tmp/cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" |grep confirm |  sed -e "s/^.*confirm=\(.*\)&amp;id=.*$/\1/" | xargs -I{} \
+curl -b /tmp/cookie.txt  -L -o ${FILE_NAME} "https://drive.google.com/uc?confirm={}&export=download&id=${FILE_ID}"
+unzip ${FILE_NAME}
+mv cifier_adam.npz model
+rm -rf ${FILE_NAME}
+
+
+FILE_ID=1qnV8_pWJofWOgsNXY3Kz5Iuh7ZrSYF_Y
+FILE_NAME=haarcascade_frontalface_default.xml
+curl -L -o ${FILE_NAME} "https://drive.google.com/uc?export=download&id=${FILE_ID}"
+mv haarcascade_frontalface_default.xml model
+
+
+FILE_ID=1azFacMd--wHUSNcwk82vYuKTKEsftxqQ
+FILE_NAME=haarcascade_frontalface_alt.xml
+curl -L -o ${FILE_NAME} "https://drive.google.com/uc?export=download&id=${FILE_ID}"
+mv haarcascade_frontalface_alt.xml model
